37b5ba6bd0ab192b8c16c724403c76c9233543bf
h.corradabravo <h.corradabravo@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-12-01 07:41:19 +0000

    remove httpuv require check on load
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/epivizr@83776 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  if (!require(httpuv, quietly=TRUE)) {
-    message("httpuv not found, needed to run epivizr")
-  }
-
   tryCatch({
     if (is.function(httpuv::startDaemonizedServer))
       options(epivizrCanDaemonize=.Platform$OS.type == "unix")
