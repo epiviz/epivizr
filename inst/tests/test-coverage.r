bacea07b79a7bfc573a17f0f1480c13b1d0c4d2b
hcorrada <hcorrada@gmail.com>
2014-05-15 23:34:32 -0400

    plot coverage from GAlignments object (simplest version)

---
+++ inst/tests/test-coverage.r
@@ -0,0 +1,11 @@
+require(ShortRead)
+
+ fl <- system.file("extdata", "ex1.bam", package="Rsamtools", mustWork=TRUE)
+gal <- readGAlignments(fl)
+gal <- renameSeqlevels(gal, c(seq1="chr1", seq2="chr2"))
+
+require(epivizr)
+mgr <- startEpiviz(chr="chr1",start=100,end=1000,verbose=TRUE)
+ms <- mgr$addMeasurements(gal, "cov", coverage.only=TRUE)
+ch <- ms$plot()
+dev <- mgr$addDevice(gal, "cov", coverage.only=TRUE)
