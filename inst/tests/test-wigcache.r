ff488b3c8ec6d1006256f9585fd4f69247622983
Hector Corrada Bravo <hcorrada@gmail.com>
2014-10-06 06:18:36 -0400

    index offseting tested on summary

--- inst/tests/test-wigcache.r
+++ inst/tests/test-wigcache.r
@@ -195,8 +195,43 @@ test_that("import.bw: hit cache again", {
 })  
 
 test_that("import.bw: zoom out", {
-    ms <- register(fl)
-  # zoom out, which should replace the whole thing
+            ms <- register(fl)
+    query <- GRanges("chr2", IRanges(500, 600))
+    ms$getRows(query, NULL)
+    
+    rng <- resize(query, width=3*width(query), fix="center")
+    obj <- import.bw(fl, which=rng, as="GRanges")
+    
+    query <- GRanges("chr2", IRanges(600,700))
+    ms$getRows(query, NULL)
+    
+                                        # new stuff on the right
+    query <- GRanges("chr2", IRanges(700, 800))
+  
+    rng2 <- setdiff(query, rng)
+    rng2 <- resize(rng2, fix="start", width=2*width(rng2))
+  
+    res <- ms$getRows(query, NULL)
+    obj2 <- import.bw(fl, which=rng2, as="GRanges")
+    obj2 <- c(obj, obj2)
+    start(rng2) <- start(rng)
+
+  # new stuff on the left
+  query <- GRanges("chr2", IRanges(300,500))
+  rng3 <- setdiff(query, rng2)
+  rng3 <- resize(rng3, fix="end", width=2*width(rng3))
+
+  res <- ms$getRows(query, NULL)
+  obj3 <- import.bw(fl, which=rng3, as="GRanges")
+  indexOffset <- length(obj3) + 1
+  obj3 <- c(obj3, obj2)
+  end(rng3) <- end(rng2)
+
+  # no change since this query is in cache
+  query <- GRanges("chr2", IRanges(500, 600))
+  res <- ms$getRows(query, NULL)
+
+                                        # zoom out, which should replace the whole thing
   query <- GRanges("chr2", IRanges(1,2000))
   rng <- resize(query, width=3*width(query), fix="center")
   if (start(rng) < 1) {
@@ -220,7 +255,7 @@ test_that("import.bw: zoom out", {
 
   expect_equal(as(ms$object, "GRanges"), obj)
   expect_equal(ms$cache$cacheRange, rng)
-  cat("sixth call done\n")
+            expect_equal(res, expectedRes)
 })
 
 test_that("cache mgmt works with summary", {
@@ -231,19 +266,20 @@ test_that("cache mgmt works with summary", {
   expect_equal(length(ms$cache$cacheRange), 0)
 
   query <- GRanges("chr2", IRanges(500, 600))
-  ms$getRows(query, NULL)
+  res <- ms$getRows(query, NULL)
 
   rng <- resize(query, width=3*width(query), fix="center")
   obj <- summary(fl, which=rng, size=ceiling(width(rng) / windowSize))[[1]]
 
   tmp <- subjectHits(GenomicRanges::findOverlaps(query, obj, select="all"))
+    
   expectedRes <- list(
       globalStartIndex=tmp[1],
       useOffset=TRUE,
       values=list(
-          id=list(tmp),
-          start=list(c(start(obj)[tmp[1]], diff(start(obj)[tmp]))),
-          end=list(c(end(obj)[tmp[1]], diff(end(obj)[tmp]))),
+          id=tmp,
+          start=c(start(obj)[tmp[1]], diff(start(obj)[tmp])),
+          end=c(end(obj)[tmp[1]], diff(end(obj)[tmp])),
           metadata=NULL))
     
   expect_equal(as(ms$object, "GRanges"), obj)
@@ -262,7 +298,7 @@ test_that("summary: hit cache", {
 
   # no change since this query is in cache
   query <- GRanges("chr2", IRanges(600,700))
-  ms$getRows(query, NULL)
+  res <- ms$getRows(query, NULL)
 
   tmp <- subjectHits(GenomicRanges::findOverlaps(query, obj, select="all"))
   expectedRes <- list(
@@ -296,7 +332,7 @@ test_that("summary: extend right", {
   rng2 <- setdiff(query, rng)
   rng2 <- resize(rng2, fix="start", width=2*width(rng2))
   
-  ms$getRows(query, NULL)
+  res <- ms$getRows(query, NULL)
   obj2 <- summary(fl, which=rng2, size=ceiling(width(rng2) / windowSize))[[1]]
   obj2 <- c(obj, obj2)
   start(rng2) <- start(rng)
@@ -344,8 +380,10 @@ test_that("summary: extend left", {
   rng3 <- setdiff(query, rng2)
   rng3 <- resize(rng3, fix="end", width=2*width(rng3))
     
-  ms$getRows(query, NULL)
+  res <- ms$getRows(query, NULL)
   obj3 <- summary(fl, which=rng3, size=ceiling(width(rng3) / windowSize))[[1]]
+    indexOffset <- length(obj3) + 1
+    
   obj3 <- c(obj3, obj2)
   end(rng3) <- end(rng2)
 
@@ -393,21 +431,23 @@ test_that("summary: hit cache again", {
     
   ms$getRows(query, NULL)
   obj3 <- summary(fl, which=rng3, size=ceiling(width(rng3) / windowSize))[[1]]
+indexOffset <- length(obj3) + 1
+            
   obj3 <- c(obj3, obj2)
   end(rng3) <- end(rng2)
 
   # no change since this query is in cache
   query <- GRanges("chr2", IRanges(500, 600))
-  ms$getRows(query, NULL)
+  res <- ms$getRows(query, NULL)
 
           tmp <- subjectHits(GenomicRanges::findOverlaps(query, obj3, select="all"))
   expectedRes <- list(
       globalStartIndex=tmp[1] - indexOffset,
       useOffset=TRUE,
       values=list(
-          id=list(tmp - indexOffset),
-          start=list(c(start(obj3)[tmp[1]], diff(start(obj3)[tmp]))),
-          end=list(c(end(obj3)[tmp[1]], diff(end(obj3)[tmp]))),
+          id=tmp - indexOffset,
+          start=c(start(obj3)[tmp[1]], diff(start(obj3)[tmp])),
+          end=c(end(obj3)[tmp[1]], diff(end(obj3)[tmp])),
           metadata=NULL))
   
 
@@ -415,6 +455,42 @@ test_that("summary: hit cache again", {
   expect_equal(ms$cache$cacheRange, rng3)
           expect_equal(res, expectedRes)
     })
+
+test_that("summary: zoom out", {
+            windowSize <- 100L
+  ms <- register(fl, windowSize=windowSize)
+  query <- GRanges("chr2", IRanges(500, 600))
+  ms$getRows(query, NULL)
+
+  rng <- resize(query, width=3*width(query), fix="center")
+  obj <- summary(fl, which=rng, size=ceiling(width(rng) / windowSize))[[1]]
+
+  query <- GRanges("chr2", IRanges(600,700))
+  ms$getRows(query, NULL)
+    
+  # new stuff on the right
+  query <- GRanges("chr2", IRanges(700, 800))
+  rng2 <- setdiff(query, rng)
+  rng2 <- resize(rng2, fix="start", width=2*width(rng2))
+  
+  ms$getRows(query, NULL)
+  obj2 <- summary(fl, which=rng2, size=ceiling(width(rng2) / windowSize))[[1]]
+  obj2 <- c(obj, obj2)
+  start(rng2) <- start(rng)
+    
+  # new stuff on the left
+  query <- GRanges("chr2", IRanges(300,500))
+  rng3 <- setdiff(query, rng2)
+  rng3 <- resize(rng3, fix="end", width=2*width(rng3))
+    
+  ms$getRows(query, NULL)
+  obj3 <- summary(fl, which=rng3, size=ceiling(width(rng3) / windowSize))[[1]]
+  obj3 <- c(obj3, obj2)
+  end(rng3) <- end(rng2)
+
+  # no change since this query is in cache
+  query <- GRanges("chr2", IRanges(500, 600))
+  ms$getRows(query, NULL)
 
   # zoom out, which should replace the whole thing
   query <- GRanges("chr2", IRanges(1,2000))
@@ -430,11 +506,20 @@ test_that("summary: hit cache again", {
   if (any(naIndex)) {
       obj <- obj[!naIndex,]
   }
-  ms$getRows(query, NULL)
+  res <- ms$getRows(query, NULL)
 
-  print(obj); print(as(ms$object, "GRanges"))
+    tmp <- subjectHits(GenomicRanges::findOverlaps(query, obj, select="all"))
+  expectedRes <- list(
+      globalStartIndex=tmp[1],
+      useOffset=TRUE,
+      values=list(
+          id=tmp,
+          start=c(start(obj)[tmp[1]], diff(start(obj)[tmp])),
+          end=c(end(obj)[tmp[1]], diff(end(obj)[tmp])),
+          metadata=NULL))
+            
   expect_equal(as(ms$object, "GRanges"), obj)
   expect_equal(ms$cache$cacheRange, rng)
-  cat("sixth call done")
+            expect_equal(res, expectedRes)
 })
 
