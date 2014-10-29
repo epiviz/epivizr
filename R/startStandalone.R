cac116058dbed1c2a1d48f27fcf28b5bb3bbb42b
Hector Corrada Bravo <hcorrada@gmail.com>
2014-10-29 08:51:48 -0400

    reorder 'startStandalone' to avoid startup bug

--- R/startStandalone.R
+++ R/startStandalone.R
@@ -1,11 +1,16 @@
 startStandalone <- function(geneInfo=NULL, geneInfoName="", seqinfo=NULL,
-                            chr="", start=1, end=1, start.args=list(), ...) {
+                            chr=NULL, start=NULL, end=NULL, start.args=list(), ...) {
+  if (is.null(geneInfo) && is.null(seqinfo)) {
+    stop("error starting standalone, one of 'geneInfo' or 'seqinfo' must not be 'NULL'")
+  }
+  
   start.args$standalone <- TRUE
   mgr <- do.call("startEpiviz",start.args)
   mgr$waitToClearRequests()
 
   tryCatch({
     if (!is.null(geneInfo)) {
+      seqinfo <- seqinfo(geneInfo)
       mgr$addSeqinfo(seqinfo(geneInfo))
     } else if (!is.null(seqinfo)) {
       mgr$addSeqinfo(seqinfo)
@@ -14,26 +19,31 @@ startStandalone <- function(geneInfo=NULL, geneInfoName="", seqinfo=NULL,
     epivizrMsg("error adding seqinfo: ", e)
   })
   mgr$waitToClearRequests()
-
-  tryCatch({
-    if (!is.null(geneInfo)) {
-      dev <- mgr$addDevice(geneInfo, geneInfoName, type="geneInfo", ...)
-      mgr$waitToClearRequests()
-      obj <- dev$getMsObject()$object
   
-      if (any(c(missing(chr), missing(start), missing(end)))) {
-        rng <- ranges(obj)[1]
-        rng <- resize(rng, width=1.5*width(rng), fix="center")
-        mgr$navigate(chr=as.character(seqnames(obj)[1]),
-                     start=start(rng), end=end(rng))
-      } else {
-        mgr$navigate(chr=chr, start=start, end=end)
-      }
-      mgr$waitToClearRequests()
-    }
+  if (missing(chr) || is.null(chr)) {
+    chr <- seqnames(seqinfo)[1]
+  }
+  
+  if (missing(start) || missing(end) || is.null(start) || is.null(end)) {
+    start <- unname(round(seqlengths(seqinfo)[chr] * .6))
+    end <- unname(round(seqlengths(seqinfo)[chr] * .7))
+  }
+  
+  tryCatch({
+    mgr$navigate(chr, start, end)
+    mgr$waitToClearRequests()
   }, error=function(e) {
-    epivizrMsg("error adding geneInfo device: ", e)
+    epivizrMsg("error navigating to starting position")
   })
   
+  if (!is.null(geneInfo)) {
+    tryCatch({
+        dev <- mgr$addDevice(geneInfo, geneInfoName, type="geneInfo", ...)
+        mgr$waitToClearRequests()
+      
+    }, error=function(e) {
+      epivizrMsg("error adding geneInfo device: ", e)
+    })
+  }
   mgr
 }
