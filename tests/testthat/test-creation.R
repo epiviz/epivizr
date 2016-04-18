e965b7ac674bb31e94d90e6f8b91c8ec4903e702
h.corradabravo <h.corradabravo@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-04-18 20:17:04 +0000

    tested EpivizApp creation
    From: Hector Corrada Bravo <hcorrada@gmail.com>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/epivizr@116494 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-creation.R
+++ tests/testthat/test-creation.R
@@ -33,3 +33,23 @@ test_that("register_chartType works as expected", {
   expect_equal(mgr$.chart_type_map$BlockChart, "epiviz.plugins.charts.BlocksTrack")
 })
 
+test_that("EpivizApp works as expected", {
+  server <- epivizrServer::createServer()
+  data_mgr <- epivizrData::createMgr(server)
+  chart_mgr <- EpivizChartMgr$new(server)
+  app <- EpivizApp$new(server=server,
+                       data_mgr=data_mgr,
+                       chart_mgr=chart_mgr)
+  
+  expect_is(app, "EpivizApp")
+  expect_is(app$server, "EpivizServer")
+
+  expect_is(app$chart_mgr, "EpivizChartMgr")
+  expect_equal(app$server, app$chart_mgr$.server)
+  
+  expect_is(app$data_mgr, "EpivizDataMgr")
+  expect_equal(app$server, app$data_mgr$.server)
+  
+  expect_equal(app$data_mgr$num_datasources(), 0)
+  expect_equal(app$chart_mgr$num_charts(), 0)
+})
