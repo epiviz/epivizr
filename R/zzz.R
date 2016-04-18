ecc458de2ac85009b77188349dc2a08102abb96c
Hector Corrada Bravo <hcorrada@gmail.com>
2016-04-18 20:15:04 +0000

    move source files that belong in other packages
    From: Hector Corrada Bravo <hcorrada@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/epivizr@116475 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  tryCatch({
-    if (is.function(httpuv::startDaemonizedServer))
-      options(epivizrCanDaemonize=.Platform$OS.type == "unix")
-  }, error=function(e) {
-    options(epivizCanDaemonize=FALSE)
-  })
-}
