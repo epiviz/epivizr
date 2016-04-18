18568f1147dea168b48331938d9ed1e0815912d7
Hector Corrada Bravo <hcorrada@gmail.com>
2016-04-18 20:16:21 +0000

    add charts implemented and tested
    From: Hector Corrada Bravo <hcorrada@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/epivizr@116487 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-add_charts.R
+++ tests/testthat/test-add_charts.R
@@ -68,81 +68,78 @@ test_that("adding a block chart works using visualize with both arguments", {
   
 })
 
-test_that("lineChart works", {
-  skip("for now")
-	sendRequest=sendRequest
-  gr <- GRanges(seqnames="chr1", ranges=IRanges(start=seq(1,100,by=25), width=1), 
-    score1=rnorm(length(seq(1,100,by=25))),score2=rnorm(length(seq(1,100,by=25))))
-  seqlengths(gr) <- c("chr1"=200000)
+test_that("adding a line chart works using visualize with just measurement", {
+  server <- epivizrServer::createServer()
+  data_mgr <- epivizrData::createMgr(server)
+  chart_mgr <- EpivizChartMgr$new(server)
   
-  mgr <- .startMGR(openBrowser=sendRequest)
+  gr <- GenomicRanges::GRanges(seqnames="chr1", 
+    ranges=IRanges::IRanges(start=seq(1,100,by=25), width=1), 
+    score1=rnorm(length(seq(1,100,by=25))),
+    score2=rnorm(length(seq(1,100,by=25))))
 
-  tryCatch({
-    if (sendRequest) wait_until(mgr$server$socketConnected)
-    msObj <- mgr$addMeasurements(gr, "ms1", sendRequest=sendRequest, type="bp")
-    msId <- msObj$getId()
-
-    if (sendRequest) wait_until(!mgr$server$requestWaiting)
-#    ms <- structure(paste0(msObj$getName(), "__score2"), names=paste0(msId,"__score2"))
-
-    if (standalone) {
-      mgr$addSeqinfo(seqinfo(gr))
-      if (sendRequest) wait_until(!mgr$server$requestWaiting)
+  ms_obj <- data_mgr$add_measurements(gr, "ms1", send_request=FALSE)
+  ms_id <- ms_obj$get_id()
+  ms <- ms_obj$get_measurements()[2]
   
-      navigate_range <- gr[1,] + 2000      
-      mgr$navigate(as.character(seqnames(navigate_range)), start(navigate_range), end(navigate_range))
-      if (sendRequest) wait_until(!mgr$server$requestWaiting)
-    }
+  chart_mgr$register_chart_type("LineChart", "epiviz.plugins.charts.LineTrack")
+  chart_obj <- chart_mgr$visualize("LineChart", measurements=ms)
+  chart_id <- chart_obj$get_id()
 
-    ms <- msObj$getMeasurements()[2]
-    chartObj <- mgr$lineChart(ms, sendRequest=sendRequest)
-    chartId <- chartObj$getId()
-
-    expect_equal(chartObj$measurements, ms)
-    expect_equal(chartObj$type, "epiviz.plugins.charts.LineTrack")
-    expect_false(is.null(mgr$chartList[[chartId]]))
-
-    if (sendRequest) wait_until(!mgr$server$requestWaiting)
-    connected <- !is.null(mgr$chartIdMap[[chartId]])
-    expect_equal(connected, sendRequest)
-  }, finally=mgr$stopServer())
+  expect_is(chart_obj, "EpivizChart")
+  expect_equal(chart_obj$.measurements, ms)
+  expect_equal(chart_obj$.type, "epiviz.plugins.charts.LineTrack")
+  expect_true(exists(chart_id, env=chart_mgr$.chart_list))
+  expect_true(is.null(chart_mgr$.chart_id_map[[chart_id]]))
 })
 
-test_that("scatterChart works", {
-  skip("for now")
-	sendRequest=sendRequest
-  sset <- makeSExp()
-  mgr <- .startMGR(openBrowser=sendRequest)
+test_that("adding a line chart works using visualize with just datasource", {
+  server <- epivizrServer::createServer()
+  data_mgr <- epivizrData::createMgr(server)
+  chart_mgr <- EpivizChartMgr$new(server)
+  
+  gr <- GenomicRanges::GRanges(seqnames="chr1", 
+                               ranges=IRanges::IRanges(start=seq(1,100,by=25), width=1), 
+                               score1=rnorm(length(seq(1,100,by=25))),
+                               score2=rnorm(length(seq(1,100,by=25))))
+  
+  ms_obj <- data_mgr$add_measurements(gr, "ms1", send_request=FALSE)
+  ms_id <- ms_obj$get_id()
+  ms <- ms_obj$get_measurements()[2]
+  
+  chart_mgr$register_chart_type("LineChart", "epiviz.plugins.charts.LineTrack")
+  chart_obj <- chart_mgr$visualize("LineChart", datasource=ms_obj)
+  chart_id <- chart_obj$get_id()
+  
+  expect_is(chart_obj, "EpivizChart")
+  expect_equal(chart_obj$.measurements, ms_obj$get_measurements())
+  expect_equal(chart_obj$.type, "epiviz.plugins.charts.LineTrack")
+  expect_true(exists(chart_id, env=chart_mgr$.chart_list))
+  expect_true(is.null(chart_mgr$.chart_id_map[[chart_id]]))
+})
 
-  tryCatch({
-    if (sendRequest) wait_until(mgr$server$socketConnected)
-    msObj <- mgr$addMeasurements(sset, "ms1", sendRequest=sendRequest, columns=c("A","B"), assay="counts2")
-    msId <- msObj$getId()
-
-    if (sendRequest) wait_until(!mgr$server$requestWaiting)
-    
-    if (standalone) {
-      mgr$addSeqinfo(seqinfo(sset))
-      if (sendRequest) wait_until(!mgr$server$requestWaiting)
-      
-      navigate_range <- rowRanges(sset)[1,] + 2000
-      mgr$navigate(as.character(seqnames(navigate_range)), start(navigate_range), end(navigate_range))
-      if (sendRequest) wait_until(!mgr$server$requestWaiting)
-    }
-    
-    x <- structure(paste0(msObj$getName(), "$A"), names=paste0(msId, "__A"))
-    y <- structure(paste0(msObj$getName(), "$B"), names=paste0(msId, "__B"))
-    ms <- msObj$getMeasurements()
-    chartObj <- mgr$scatterChart(x=ms[[1]], y=ms[[2]],sendRequest=sendRequest)
-    chartId <- chartObj$getId()
-
-    expect_equal(chartObj$measurements, ms)
-    expect_equal(chartObj$type, "epiviz.plugins.charts.ScatterPlot")
-    expect_false(is.null(mgr$chartList[[chartId]]))
-
-    if (sendRequest) wait_until(!mgr$server$requestWaiting)
-    connected <- !is.null(mgr$chartIdMap[[chartId]])
-    expect_equal(connected, sendRequest)
-  }, finally=mgr$stopServer())
+test_that("adding a line chart works using visualize with both arguments", {
+  server <- epivizrServer::createServer()
+  data_mgr <- epivizrData::createMgr(server)
+  chart_mgr <- EpivizChartMgr$new(server)
+  
+  gr <- GenomicRanges::GRanges(seqnames="chr1", 
+                               ranges=IRanges::IRanges(start=seq(1,100,by=25), width=1), 
+                               score1=rnorm(length(seq(1,100,by=25))),
+                               score2=rnorm(length(seq(1,100,by=25))))
+  
+  ms_obj <- data_mgr$add_measurements(gr, "ms1", send_request=FALSE)
+  ms_id <- ms_obj$get_id()
+  ms <- ms_obj$get_measurements()[2]
+  
+  chart_mgr$register_chart_type("LineChart", "epiviz.plugins.charts.LineTrack")
+  chart_obj <- chart_mgr$visualize("LineChart", measurements=ms, datasource=ms_obj)
+  chart_id <- chart_obj$get_id()
+  
+  expect_is(chart_obj, "EpivizChart")
+  expect_equal(chart_obj$.measurements, ms)
+  expect_equal(chart_obj$.type, "epiviz.plugins.charts.LineTrack")
+  expect_true(exists(chart_id, env=chart_mgr$.chart_list))
+  expect_true(is.null(chart_mgr$.chart_id_map[[chart_id]]))
 })
 
