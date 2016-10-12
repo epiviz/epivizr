93a735cddcc98e90ace63c6da48bfd2e8be599bd
Hector Corrada Bravo <hcorrada@gmail.com>
2016-10-12 20:50:03 +0000

    Various fixes and updates
    * Fix documentation issue in 'EpivizApp-class'
    * Add documentation for 'show-EpivizChart' method
    * get 'datasource_name' from 'data_object' argument if missing on 'plot'
    * version bump
    
    From: hcorrada <hcorrada@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/epivizr@122299 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-plot_app.R
+++ tests/testthat/test-plot_app.R
@@ -1,5 +1,21 @@
 context("app plot methods")
 
+test_that("plot works without datasource name", {
+  server <- epivizrServer::createServer()
+  data_mgr <- epivizrData::createMgr(server)
+  chart_mgr <- EpivizChartMgr$new(server)
+  app <- EpivizApp$new(server=server,
+                       data_mgr=data_mgr,
+                       chart_mgr=chart_mgr)
+  
+  gr <- GenomicRanges::GRanges(seqnames="chr1", ranges=IRanges::IRanges(start=1:10, width=1))
+  chart_mgr$register_chart_type("BlocksTrack", "epiviz.plugins.charts.BlocksTrack")
+  chart_obj <- app$plot(gr, send_request=FALSE)
+
+  ms_obj <- app$get_ms_object(chart_obj)
+  expect_equal(ms_obj$get_name(), "gr")
+})
+
 test_that("plot block works", {
   server <- epivizrServer::createServer()
   data_mgr <- epivizrData::createMgr(server)
