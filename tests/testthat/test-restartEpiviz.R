3668387ce8ae04a832e783ee7176deae2f37fdc4
Brian Gottfried <briangottfried@me.com>
2017-01-28 14:05:25 -0500

    Fixed infinite recursion in save method and added test for restartEpiviz.

---
+++ tests/testthat/test-restartEpiviz.R
@@ -0,0 +1,27 @@
+context("restart epiviz")
+
+test_that("restartEpiviz restarts connection and workspace", {
+  app <- startEpiviz(open_browser=FALSE,
+                     port=7123L,
+                     static_site_path=".",
+                     daemonized=TRUE,
+                     verbose=TRUE)
+  file_name <- "test-restartEpiviz.rda"
+  expect_true(!app$server$is_closed())
+  expect_true(!file.exists(file_name))
+  app$save(file_name)
+  expect_true(file.exists(file_name))
+  expect_true(app$server$is_closed())
+  
+  app <- restartEpiviz(file_name)
+  expect_true(!app$server$is_closed())
+  expect_is(app, "EpivizApp")
+  expect_is(app$server, "EpivizServer")
+  expect_is(app$chart_mgr, "EpivizChartMgr")
+  expect_is(app$data_mgr, "EpivizDataMgr")
+  
+  app$stop_app()
+  app$server$stop_server()
+  file.remove(file_name)
+})
+
