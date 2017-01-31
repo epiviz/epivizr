7977f64e92a6140d0b1218f81855e23750c7fb92
Brian Gottfried <briangottfried@me.com>
2017-01-30 19:12:04 -0500

    Added open_browser parameter and check in the body.

--- R/restartEpiviz.R
+++ R/restartEpiviz.R
@@ -1,7 +1,7 @@
 #' Restart epiviz app connection and workspace.
 #'
 #' @param file (character) The name of the file that holds the EpivizApp object to be restarted, ending in .rda.
-#'
+#' @param open_browser (logical) browse to the epiviz URL before exiting function.
 #' @return An object of class \code{\link{EpivizApp}}
 #'
 #' @examples
@@ -9,7 +9,7 @@
 #' app <- restartEpiviz(file="app.rda")
 #'
 #' @export
-restartEpiviz <- function(file) {
+restartEpiviz <- function(file, open_browser=TRUE) {
 
   if (!file.exists(file)) {
     stop("File does not exist")
@@ -18,14 +18,17 @@ restartEpiviz <- function(file) {
   load(file=file)
   saved_app <- .self
 
-  saved_app$server$start_server()
-  saved_app$.open_browser()
-  saved_app$service()
+  .self$server$start_server()
+  if (open_browser) {
+    .self$.open_browser()
+  }
+  .self$service()
 
   chart_ids <- ls(envir=saved_app$chart_mgr$.chart_list)
   for (id in chart_ids) {
     chart_obj <- saved_app$chart_mgr$.get_chart_object(id)
-    saved_app$chart_mgr$add_chart(chart_obj)
+    .self$chart_mgr$add_chart(chart_obj)
   }
-  return(saved_app)
+  
+  return(.self)
 }
