6e4cee092b4f000d8aac4d2e0696b8a6922a3658
h.corradabravo <h.corradabravo@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-03-12 20:01:00 +0000

    Add ability to save and restart an epivizr session
    - add restart function, add save method, include them in vignette
    - added test for restartEpiviz() function.
    - saved file before closing server to perserve charts in .rda
    - restartEpiviz will look for data if not in file
    - if the EpivizApp session is saved without its data, restartEpiviz will now check if data is loaded in global environment and add the measurements.
    - test for restarting Epiviz without data saved in file.
    - plot will deparse source name and pass it into new add_measurements param.
    - data_object was being deparsed in add measurements and was losing its datasource name. To fix this, added a parameter in data mgr's add measurements to pass down the original name of the source.
    - these functions allow restart Epiviz to request the charts are redrawn to the UI.
    - open browser after uploading data
    - allows open browser to initialize before re-adding charts
    - save method adds data back if user epiviz app if user wants to continue session
    - checks if data source is an expression: if data source is an expression and failed to evaluate from environment, this will print the datasource name without the expression.
    
    From: Morgan Walter <Morgan Walter>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/epivizr@127331 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/restartEpiviz.R
@@ -0,0 +1,97 @@
+#' Restart epiviz app connection and workspace.
+#'
+#' @param file (character) The name of the file that holds the EpivizApp object to be restarted, ending in .rda.
+#' @param open_browser (logical) browse to the epiviz URL before exiting function.
+#' @return An object of class \code{\link{EpivizApp}}
+#'
+#' @examples
+#' # see package vignette for example usage
+#' app <- restartEpiviz(file="app.rda")
+#'
+#' @export
+restartEpiviz <- function(file, open_browser=TRUE, host=NULL) {
+  
+  if (!file.exists(file)) {
+    stop("File does not exist")
+  }
+  
+  load(file=file)
+  app <- .self
+  app$server$start_server()    
+  app$service()
+  
+  if(!is.null(host)) {
+    app$.url_parms$host = host
+  }
+  
+  ms_ids <- ls(envir=app$data_mgr$.ms_list)
+  data_not_in_environment <- NULL
+  
+  # If file is saved without data, this will
+  # update data into Epiviz App if it exists
+  # in environment
+  for (id in ms_ids){
+    ms_obj <- app$data_mgr$.get_ms_object(id)
+    
+    if (is.null(ms_obj$.object)) {
+      new_obj <- tryCatch({
+        eval(parse(text=ms_obj$get_source_name()))
+      }, error = function(e) {
+        # instead of stopping here, this accumulates 
+        # names of all datasources not in environment
+        datasource_name <- ms_obj$get_source_name()
+        
+        expression_pattern <- "\\[[[:print:]]*\\]"
+        is_datasource_an_expression <- grepl(expression_pattern, datasource_name)
+        
+        if (is_datasource_an_expression) {
+          datasource_name <- gsub(expression_pattern, "", datasource_name)
+        }
+       
+        data_not_in_environment <<- c(data_not_in_environment, datasource_name)    
+        
+        return(NULL)
+      })
+      
+      if (!is.null(new_obj)) {
+        ms_obj$update(new_obj)
+      }
+      
+    } 
+  }
+  
+  if (!is.null(data_not_in_environment)) {
+    datasource_names <- paste(data_not_in_environment, collapse=", ")
+    app$server$stop_server()
+    stop("Load data in environment before restarting: ", datasource_names,".")
+  }
+  
+  if (open_browser) {
+    app$.open_browser()
+    
+    callback <- function(response_data) {
+      if (app$server$.verbose){
+        if(response_data$success){
+          cat("UI is READY \n")       
+        } else {
+          cat("UI is NOT READY \n")
+        }
+      }
+    }
+    
+    request_data <- list(action="uiStatus")
+    app$server$send_request(request_data, callback)
+  
+    tryCatch({
+      app$server$wait_to_clear_requests()
+    }, error = function(e) {
+      app$server$stop_server()
+      stop(e)
+    })
+    
+  }
+  
+  app$chart_mgr$redraw_charts()
+  
+  return(app)
+}
